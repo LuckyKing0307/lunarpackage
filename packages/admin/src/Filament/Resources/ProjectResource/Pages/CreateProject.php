db9f054829d01c2781393187f02da8b87078576d
LuckyKing0307 <jasaergasheva@gmail.com>
2025-08-14 15:37:16 +0500

    Update

---
+++ packages/admin/src/Filament/Resources/ProjectResource/Pages/CreateProject.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lunar\Admin\Filament\Resources\ProjectResource\Pages;
+
+use Filament\Resources\Pages\CreateRecord;
+use Lunar\Admin\Filament\Resources\ProjectResource;
+
+class CreateProject extends CreateRecord
+{
+    protected static string $resource = ProjectResource::class;
+}
